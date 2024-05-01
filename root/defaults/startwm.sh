7c52486672de3b1c5f3e44d74c634e3501885259
tibor309 <tibormercs8@gmail.com>
2024-05-01 19:25:03 +0200

    Changed setup

--- root/defaults/startwm.sh
+++ root/defaults/startwm.sh
@@ -2,5 +2,15 @@
 
 setterm blank 0
 setterm powerdown 0
-#gsettings set org.mate.Marco.general compositing-manager false
+
+# set folder locations
+xdg-user-dirs-update --set DESKTOP /config/Desktop
+xdg-user-dirs-update --set DOCUMENTS /config/Documents
+xdg-user-dirs-update --set DOWNLOAD /config/Downloads
+xdg-user-dirs-update --set MUSIC /config/Music
+xdg-user-dirs-update --set PICTURES /config/Pictures
+xdg-user-dirs-update --set PUBLICSHARE /config/Public
+xdg-user-dirs-update --set TEMPLATES /config/Templates
+xdg-user-dirs-update --set VIDEOS /config/Videos
+
 /usr/bin/cinnamon-session > /dev/null 2>&1
