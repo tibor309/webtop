4ec653bfb6f24eff2f0dad2dc1e0423c953b3ebc
Tibor Mercs <tibormercs8@gmail.com>
2024-07-16 13:44:15 +0000

    Added changes from the ubuntu-cinnamon branch

--- root/defaults/startwm.sh
+++ root/defaults/startwm.sh
@@ -2,5 +2,30 @@
 
 setterm blank 0
 setterm powerdown 0
-#gsettings set org.mate.Marco.general compositing-manager false
+
+# Enable Nvidia GPU support if detected
+if which nvidia-smi; then
+  export LIBGL_KOPPER_DRI2=1
+  export MESA_LOADER_DRIVER_OVERRIDE=zink
+  export GALLIUM_DRIVER=zink
+fi
+
+if [ ! -f "$HOME/.firstsetup" ]; then
+    mkdir -p $HOME/{Desktop,Documents,Downloads,Music,Pictures,Public,Templates,Videos}
+    chown abc:abc $HOME/{Desktop,Documents,Downloads,Music,Pictures,Public,Templates,Videos}
+
+    xdg-user-dirs-update --set DESKTOP $HOME/Desktop
+    xdg-user-dirs-update --set DOCUMENTS $HOME/Documents
+    xdg-user-dirs-update --set DOWNLOAD $HOME/Downloads
+    xdg-user-dirs-update --set MUSIC $HOME/Music
+    xdg-user-dirs-update --set PICTURES $HOME/Pictures
+    xdg-user-dirs-update --set PUBLICSHARE $HOME/Public
+    xdg-user-dirs-update --set TEMPLATES $HOME/Templates
+    xdg-user-dirs-update --set VIDEOS $HOME/Videos
+
+    touch $HOME/.firstsetup
+fi
+# set folder locations
+
+
 /usr/bin/cinnamon-session > /dev/null 2>&1
