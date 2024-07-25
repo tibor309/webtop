9b4058c670623d95ff2ade4c8f6cacd7a7f7d121
Tibor Mercs <tibormercs8@gmail.com>
2024-07-25 14:34:26 +0000

    Moved creation of user folders into startwm script

--- root/defaults/startwm.sh
+++ root/defaults/startwm.sh
@@ -12,18 +12,24 @@ gsettings set org.gnome.desktop.session idle-delay 0
 # set session
 export XDG_SESSION_TYPE=x11
 export DESKTOP_SESSION=gnome-xorg
-#export GNOME_SHELL_SESSION_MODE=ubuntu
 export XDG_CURRENT_DESKTOP=GNOME
 
-# set folder locations
-xdg-user-dirs-update --set DESKTOP /config/Desktop
-xdg-user-dirs-update --set DOCUMENTS /config/Documents
-xdg-user-dirs-update --set DOWNLOAD /config/Downloads
-xdg-user-dirs-update --set MUSIC /config/Music
-xdg-user-dirs-update --set PICTURES /config/Pictures
-xdg-user-dirs-update --set PUBLICSHARE /config/Public
-xdg-user-dirs-update --set TEMPLATES /config/Templates
-xdg-user-dirs-update --set VIDEOS /config/Videos
+# create user folders
+if [ ! -f "/config/.firstsetup" ]; then
+    mkdir -p /config/{Desktop,Documents,Downloads,Music,Pictures,Public,Templates,Videos}
+    chown abc:abc /config/{Desktop,Documents,Downloads,Music,Pictures,Public,Templates,Videos}
+
+    xdg-user-dirs-update --set DESKTOP /config/Desktop
+    xdg-user-dirs-update --set DOCUMENTS /config/Documents
+    xdg-user-dirs-update --set DOWNLOAD /config/Downloads
+    xdg-user-dirs-update --set MUSIC /config/Music
+    xdg-user-dirs-update --set PICTURES /config/Pictures
+    xdg-user-dirs-update --set PUBLICSHARE /config/Public
+    xdg-user-dirs-update --set TEMPLATES /config/Templates
+    xdg-user-dirs-update --set VIDEOS /config/Videos
+
+    touch /config/.firstsetup
+fi
 
 export $(dbus-launch)
 export XDG_DATA_DIRS=/var/lib/flatpak/exports/share:/config/.local/share/flatpak/exports/share:/usr/local/share:/usr/share
