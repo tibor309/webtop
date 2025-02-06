a782fbcfaae7bc84bfa33925f5a335ed8fb8ef4b
Tibor <tibordev@protonmail.com>
2025-02-06 15:48:05 +0000

    Changed userdirs

--- root/defaults/startwm.sh
+++ root/defaults/startwm.sh
@@ -13,26 +13,21 @@ gsettings set org.gnome.desktop.session idle-delay 0
 export XDG_SESSION_TYPE=x11
 export DESKTOP_SESSION=zorin-xorg
 export XDG_CURRENT_DESKTOP=zorin:GNOME
-
-# create user folders
-if [ ! -f "$HOME/.firstsetup" ]; then
-    mkdir -p $HOME/{Desktop,Documents,Downloads,Music,Pictures,Public,Templates,Videos}
-    chown abc:abc $HOME/{Desktop,Documents,Downloads,Music,Pictures,Public,Templates,Videos}
-
-    xdg-user-dirs-update --set DESKTOP $HOME/Desktop
-    xdg-user-dirs-update --set DOCUMENTS $HOME/Documents
-    xdg-user-dirs-update --set DOWNLOAD $HOME/Downloads
-    xdg-user-dirs-update --set MUSIC $HOME/Music
-    xdg-user-dirs-update --set PICTURES $HOME/Pictures
-    xdg-user-dirs-update --set PUBLICSHARE $HOME/Public
-    xdg-user-dirs-update --set TEMPLATES $HOME/Templates
-    xdg-user-dirs-update --set VIDEOS $HOME/Videos
-
-    touch $HOME/.firstsetup
-fi
-
-export $(dbus-launch)
 export XDG_DATA_DIRS=/usr/local/share:/usr/share
+export $(dbus-launch)
+
+# set user folders
+mkdir -p /config/{Desktop,Documents,Downloads,Music,Pictures,Public,Templates,Videos}
+
+export XDG_DESKTOP_DIR=/config/Desktop
+export XDG_DOCUMENTS_DIR=/config/Documents
+export XDG_DOWNLOAD_DIR=/config/Downloads
+export XDG_MUSIC_DIR=/config/Music
+export XDG_PICTURES_DIR=/config/Pictures
+export XDG_PUBLICSHARE_DIR=/config/Public
+export XDG_TEMPLATES_DIR=/config/Templates
+export XDG_VIDEOS_DIR=/config/Videos
+xdg-user-dirs-update
 
 # launch DE
 /usr/bin/gnome-shell --x11 -r > /dev/null 2>&1
