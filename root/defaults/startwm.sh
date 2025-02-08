c1406c3c7246c7160cf4f23a323a384f11055ed3
tibormercs8 <tibormercs8@gmail.com>
2025-02-08 20:37:28 +0000

    Changed userdirs

--- root/defaults/startwm.sh
+++ root/defaults/startwm.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-setterm blank 0
-setterm powerdown 0
-
 # Enable Nvidia GPU support if detected
 if which nvidia-smi; then
   export LIBGL_KOPPER_DRI2=1
@@ -10,28 +7,26 @@ if which nvidia-smi; then
   export GALLIUM_DRIVER=zink
 fi
 
+setterm blank 0
+setterm powerdown 0
+
 # disable screen lock
-if [ ! -f $HOME/.config/kscreenlockerrc ]; then
-  kwriteconfig5 --file $HOME/.config/kscreenlockerrc --group Daemon --key Autolock false
+if [ ! -f /config/.config/kscreenlockerrc ]; then
+  kwriteconfig5 --file /config/.config/kscreenlockerrc --group Daemon --key Autolock false
 fi
 
-# create user folders
-if [ ! -f "$HOME/.firstsetup" ]; then
-    mkdir -p $HOME/{Desktop,Documents,Downloads,Music,Pictures,Public,Templates,Videos}
-    chown abc:abc $HOME/{Desktop,Documents,Downloads,Music,Pictures,Public,Templates,Videos}
+# set user folders
+mkdir -p /config/{Desktop,Documents,Downloads,Music,Pictures,Public,Templates,Videos}
 
-    xdg-user-dirs-update --set DESKTOP $HOME/Desktop
-    xdg-user-dirs-update --set DOCUMENTS $HOME/Documents
-    xdg-user-dirs-update --set DOWNLOAD $HOME/Downloads
-    xdg-user-dirs-update --set MUSIC $HOME/Music
-    xdg-user-dirs-update --set PICTURES $HOME/Pictures
-    xdg-user-dirs-update --set PUBLICSHARE $HOME/Public
-    xdg-user-dirs-update --set TEMPLATES $HOME/Templates
-    xdg-user-dirs-update --set VIDEOS $HOME/Videos
-
-    touch $HOME/.firstsetup
-fi
-
+export XDG_DESKTOP_DIR=/config/Desktop
+export XDG_DOCUMENTS_DIR=/config/Documents
+export XDG_DOWNLOAD_DIR=/config/Downloads
+export XDG_MUSIC_DIR=/config/Music
+export XDG_PICTURES_DIR=/config/Pictures
+export XDG_PUBLICSHARE_DIR=/config/Public
+export XDG_TEMPLATES_DIR=/config/Templates
+export XDG_VIDEOS_DIR=/config/Videos
+xdg-user-dirs-update
 
 # launch de
 /usr/bin/dbus-launch /usr/bin/startplasma-x11 > /dev/null 2>&1
