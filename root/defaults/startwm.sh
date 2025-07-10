ca1a1b23ac222f1d12433681e7059a0acf16cba5
Tibor <tibormercs8@gmail.com>
2025-07-10 17:13:37 +0000

    Updated start script

--- root/defaults/startwm.sh
+++ root/defaults/startwm.sh
@@ -1,27 +1,31 @@
 #!/bin/bash
 
-# enable nvidia gpu support if detected
+# Enable Nvidia GPU support if detected
 if which nvidia-smi; then
   export LIBGL_KOPPER_DRI2=1
   export MESA_LOADER_DRIVER_OVERRIDE=zink
   export GALLIUM_DRIVER=zink
 fi
 
+# Disable sleep and power off
 setterm blank 0
 setterm powerdown 0
 
-# set user folders
-mkdir -p /config/{Desktop,Documents,Downloads,Music,Pictures,Public,Templates,Videos}
+# Create user directories
+for dir in Desktop Documents Downloads Music Pictures Public Templates Videos; do
+  [ -d "${HOME}/$dir" ] || mkdir -p "${HOME}/$dir"
+done
 
-export XDG_DESKTOP_DIR=/config/Desktop
-export XDG_DOCUMENTS_DIR=/config/Documents
-export XDG_DOWNLOAD_DIR=/config/Downloads
-export XDG_MUSIC_DIR=/config/Music
-export XDG_PICTURES_DIR=/config/Pictures
-export XDG_PUBLICSHARE_DIR=/config/Public
-export XDG_TEMPLATES_DIR=/config/Templates
-export XDG_VIDEOS_DIR=/config/Videos
+# Set up XDG user directories
+export XDG_DESKTOP_DIR="$HOME/Desktop"
+export XDG_DOCUMENTS_DIR="$HOME/Documents"
+export XDG_DOWNLOAD_DIR="$HOME/Downloads"
+export XDG_MUSIC_DIR="$HOME/Music"
+export XDG_PICTURES_DIR="$HOME/Pictures"
+export XDG_PUBLICSHARE_DIR="$HOME/Public"
+export XDG_TEMPLATES_DIR="$HOME/Templates"
+export XDG_VIDEOS_DIR="$HOME/Videos"
 xdg-user-dirs-update
 
-# launch de
-/usr/bin/xfce4-session > /dev/null 2>&1
+# Stat DE
+/usr/bin/dbus-launch /usr/bin/xfce4-session > /dev/null 2>&1
